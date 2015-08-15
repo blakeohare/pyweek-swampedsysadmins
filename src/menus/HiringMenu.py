b1cf9d4bf2f54bfc43da3f22c622eb6a864bc181
Blake O'Hare <mspaintmaestro@gmail.com>
2015-08-15 01:14:40 -0700

    Hiring menu is fully implemented.

--- src/menus/HiringMenu.py
+++ src/menus/HiringMenu.py
@@ -9,6 +9,8 @@ class HiringMenu:
 		self.parent = wrapper_menu
 		self.tab = 'HR'
 		self.color = (40, 60, 100)
+		self.staff_member = None
+		self.hired = False
 		
 		
 	
@@ -16,7 +18,11 @@ class HiringMenu:
 		if self.elements == None:
 			from src.Staff import Staff
 			staff_id = len(self.model.staff)
-			person = Staff(staff_id)
+			if self.staff_member == None:
+				person = Staff(staff_id)
+				self.staff_member = person
+			else:
+				person = self.staff_member
 			person.direction = 's'
 			person.moving = True
 			images = []
@@ -33,7 +39,7 @@ class HiringMenu:
 			
 			y = 150
 			col0 = 20
-			col1 = 200
+			col1 = 300
 			elements.append(create_ui_text('Asking Salary:', 'white', col0, y))
 			elements.append(create_ui_text('$100 / game', 'green', col1, y))
 			
@@ -41,6 +47,21 @@ class HiringMenu:
 			elements.append(create_ui_text('Current Payroll:', 'white', col0, y))
 			elements.append(create_ui_text('-$' + str(staff_id * 100), 'red', col1, y))
 			
+			y += 50
+			elements.append(create_ui_button('Hire', self.hire, 100, y, 100, 24, self.can_hire))
+			
 			self.elements = elements
 		return self.elements
 	
+	def can_hire(self):
+		return not self.hired
+	
+	def hire(self):
+		from src.Staff import Staff
+		new_hire = Staff(self.staff_member.id)
+		new_hire.x = 32 * 5
+		new_hire.y = 32 * 5
+		self.model.staff.append(new_hire)
+		self.hired = True
+		self.elements = None
+		self.parent.elements = None
